c61ad1a0d3733cbe4b4c3cacd74be90ca3a22c86
Louvre Chow <louvrecly@gmail.com>
2020-01-27 18:09:57 +0800

    master: Add ApplePayBloc to provider in home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../blocs/bloc.dart';
 import '../blocs/google_pay_bloc.dart';
+import '../blocs/apple_pay_bloc.dart';
 import '../components/pay_button.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -21,6 +22,10 @@ class _MyHomePageState extends State<MyHomePage> {
       providers: [
         Provider(
           create: (BuildContext context) => GooglePayBloc(environment: 'test'),
+          dispose: (BuildContext context, Bloc bloc) => bloc.dispose(),
+        ),
+        Provider(
+          create: (BuildContext context) => ApplePayBloc(environment: 'test'),
           dispose: (BuildContext context, Bloc bloc) => bloc.dispose(),
         ),
       ],
